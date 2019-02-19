a39f2fee1e43b6528ce17ae2ce544edd719ef199
norbert <norbert.zielinski@bzwbk.pl>
2019-02-19 15:32:59 +0100

    Rozwinięcie RestController'a o wyszukiwanie po atrybutach

--- book-library/src/main/java/com/blackbeast/booklibrary/repository/BookRepository.java
+++ book-library/src/main/java/com/blackbeast/booklibrary/repository/BookRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 import java.util.Collection;
 
@@ -41,9 +42,47 @@ public class BookRepository {
     }
 
     public Collection<Book> getBooksByAuthor(String authorName){
-        return em.createQuery("from Book b WHERE b.author.name = :authorName", Book.class)
-                .setParameter("authorName", authorName)
+        return em.createQuery("from Book b WHERE LOWER(b.author.name) LIKE CONCAT('%',:authorName,'%')", Book.class)
+                .setParameter("authorName", authorName.toLowerCase())
                 .getResultList();
+    }
+
+    public Collection<Book> getBooks(Integer year, String publisher, String isbn) {
+        String query = "from Book b";
+        String conditions = "";
+
+        if(year != null)
+            conditions += "b.year = :year";
+
+        if(publisher != null)
+            if(conditions.isEmpty())
+                conditions += "b.publisher = :publisher";
+            else
+                conditions += " AND b.publisher = :publisher";
+
+        if(isbn != null)
+            if(isbn.isEmpty())
+                conditions += "b.isbn = :isbn";
+            else
+                conditions += " AND b.isbn = :isbn";
+
+        if(!conditions.isEmpty())
+            query += " WHERE " + conditions;
+
+        System.out.println(query);
+
+        TypedQuery typedQuery = em.createQuery(query, Book.class);
+
+        if(year != null)
+            typedQuery.setParameter("year", year);
+
+        if(publisher != null)
+            typedQuery.setParameter("publisher", publisher);
+
+        if(isbn != null)
+            typedQuery.setParameter("isbn", isbn);
+
+        return typedQuery.getResultList();
     }
 
 }
