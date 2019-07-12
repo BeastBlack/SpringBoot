3574ecb53fa9238c62c95b83e6de0fca76d4eba3
norbert <norbert.zielinski@bzwbk.pl>
2019-07-12 13:42:56 +0200

    Edycja profilu użytkownika, zarządzanie użytkwonikami

--- book-library/src/main/java/com/blackbeast/booklibrary/controllers/UserController.java
+++ book-library/src/main/java/com/blackbeast/booklibrary/controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -25,20 +26,44 @@ public class UserController {
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String registerUser(Model model, @Valid User user, BindingResult bindingResult) {
+    public String saveUser(Model model, @Valid User user, BindingResult bindingResult) {
         if(bindingResult.hasErrors()) {
             return "user";
         }else
-            if(userService.getUser(user.getUsername()) != null) {
-                model.addAttribute("user", user);
-                model.addAttribute("showMessage", "REGISTER_EXISTS");
-                return "user";
-            } else {
+            if(user.getId() != null) { //edycja istniejącego użytkownika
                 userService.saveUser(user);
                 model.addAttribute("user", new User());
-                model.addAttribute("showMessage", "REGISTER_OK");
+                model.addAttribute("showMessage", "EDIT_OK");
                 return "user";
-            }
+            }else //nowy użytkownik
+                if(userService.getUser(user.getUsername()) != null) {
+                    model.addAttribute("user", user);
+                    model.addAttribute("showMessage", "REGISTER_EXISTS");
+                    return "user";
+                } else {
+                    userService.saveUser(user);
+                    model.addAttribute("user", new User());
+                    model.addAttribute("showMessage", "REGISTER_OK");
+                    return "user";
+                }
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public String editUser(Model model) {
+        User user = userService.getLoggedUser();
+
+        model.addAttribute("user", user);
+
+        return "user";
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public String showUsers(Model model) {
+        List<User> users = userService.getAll();
+
+        model.addAttribute("users", users);
+        model.addAttribute("loggedUser", userService.getLoggedUser());
+        return "users";
     }
 
 
